8b2cd51c876f4d12a310b8f61c5ac30bd8fb7962
Rachel Veer <rachelyoubish@gmail.com>
2020-11-18 16:31:19 -0600

    Fixed wrong spdlog api call.

--- DXE/src/DXE/Log.h
+++ DXE/src/DXE/Log.h
@@ -24,11 +24,11 @@ namespace DXE {
 #define DXE_CORE_INFO(...)     ::DXE::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define DXE_CORE_WARN(...)     ::DXE::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define DXE_CORE_ERROR(...)    ::DXE::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define DXE_CORE_FATAL(...)    ::DXE::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define DXE_CORE_FATAL(...)    ::DXE::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define DXE_TRACE(...)         ::DXE::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define DXE_INFO(...)          ::DXE::Log::GetClientLogger()->info(__VA_ARGS__)
 #define DXE_WARN(...)          ::DXE::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define DXE_ERROR(...)         ::DXE::Log::GetClientLogger()->error(__VA_ARGS__)
-#define DXE_FATAL(...)         ::DXE::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define DXE_FATAL(...)         ::DXE::Log::GetClientLogger()->critical(__VA_ARGS__)
