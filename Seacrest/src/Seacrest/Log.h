e33dfab94a07d152afac302f574a407f8873bd69
Rachel Veer <rachelyoubish@gmail.com>
2020-12-02 09:02:25 -0600

    Logger macro name changed.

--- Seacrest/src/Seacrest/Log.h
+++ Seacrest/src/Seacrest/Log.h
@@ -26,11 +26,11 @@ namespace Seacrest {
 #define SEACREST_CORE_INFO(...)     ::Seacrest::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define SEACREST_CORE_WARN(...)     ::Seacrest::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define SEACREST_CORE_ERROR(...)    ::Seacrest::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define SEACREST_CORE_FATAL(...)    ::Seacrest::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define SEACREST_CORE_CRITICAL(...)    ::Seacrest::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define SEACREST_TRACE(...)         ::Seacrest::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define SEACREST_INFO(...)          ::Seacrest::Log::GetClientLogger()->info(__VA_ARGS__)
 #define SEACREST_WARN(...)          ::Seacrest::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define SEACREST_ERROR(...)         ::Seacrest::Log::GetClientLogger()->error(__VA_ARGS__)
-#define SEACREST_FATAL(...)         ::Seacrest::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define SEACREST_CRITICAL(...)         ::Seacrest::Log::GetClientLogger()->critical(__VA_ARGS__)
