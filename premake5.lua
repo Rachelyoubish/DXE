7efec8572b60e1de1d6e16af70c26c543dce56fb
Rachel Veer <rachelyoubish@gmail.com>
2020-12-15 21:49:51 -0600

    Premake build script: Include project resource file.

--- premake5.lua
+++ premake5.lua
@@ -98,7 +98,8 @@ project "Sandbox"
     files
     {
         "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/Resources/**.rc"
     }
 
     includedirs
